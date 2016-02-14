1af6e16a894d9394e8c91943a8b0fc8032066774
davidmarkclements <huperekchuno@googlemail.com>
2016-02-14 21:36:40 +0000

    add profile_1ms.d

---
+++ profile_1ms.d
@@ -0,0 +1,11 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option quiet
+#pragma D option switchrate=1000hz
+
+profile-1ms /pid == $target/ {
+  /* Sampling every 1ms therefore also recording timestamp at ms resolution */
+  printf("%s %d %d: %s:", execname, pid, timestamp / 1000000, probename);
+  ustack(10000);
+  printf("\n");
+}
