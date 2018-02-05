d748dda080893a8d7e698a920ad03e0725094652
davidmarkclements <huperekchuno@googlemail.com>
2018-02-05 19:16:05 +0000

    better error output

--- cmd.js
+++ cmd.js
@@ -9,8 +9,15 @@ var pump = require('pump')
   --keep-addr -k [false]
  */
 
+if (!args._[0]) {
+  console.error('Provide PID of the process when the stacks were captured')
+  process.exit(1)
+}
+
 var stream = require('./')(Object.assign(args, {
   pid: args._[0]
 }))
+
+console.log(stream)
 
 pump(process.stdin, stream, process.stdout)
